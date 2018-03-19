2f6d50aa3496be00940b517b323b32792bff7e29
Patrick Watrous <patrickwatrous@live.com>
2018-03-19 02:53:49 -0400

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import LandingPage from './screens/LandingPage/LandingPage';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<LandingPage />, document.getElementById('root'));
-registerServiceWorker();
