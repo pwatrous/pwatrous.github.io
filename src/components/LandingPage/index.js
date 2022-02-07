45aae3568707e9f1b0ddf5bbae47cd0241ebe0f8
Patrick Watrous <patrickwatrous@live.com>
2022-02-06 23:16:27 -0500

    initial setup of app

---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LandingPageContainer = styled.div`
+    width: 100vw;
+    height: 500px;
+    background-color: #B5C9FF;
+`;
+
+const LandingPage = () => {
+  return (
+    <LandingPageContainer>
+      Landing Page
+    </LandingPageContainer>
+  );
+}
+
+export default LandingPage;
