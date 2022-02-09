3837ad78640937f3657d3f2cc7afd7878ba94b5a
Patrick Watrous <patrickwatrous@live.com>
2022-02-09 06:43:58 -0500

    rest of timeline UI

--- src/components/TimelinePage/index.js
+++ src/components/TimelinePage/index.js
@@ -6,55 +6,119 @@ import neu from '../../assets/northeastern.png';
 import harvard from '../../assets/Harvard-Catalyst.png';
 import loomis from '../../assets/loomis.png';
 
-const TimelinePageContainer = styled(Grid)`
+const TimelinePageContainer = styled.div`
   height: 100vh;
   background-color: #B5C9FF;
   width: 100vw;
 `;
 const TimelineCard = styled(Card)`
   height: 150px;
-  margin: 50px 0;
+  margin: 40px 0;
 `;
 const CardContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: flex-start;
+  justify-content: space-between;
   height: 100%;
-  padding-left: 30px;
+  padding: 0 30px;
 `;
 const TimelineImg = styled.img`
   width: 7vw;
+`;
+const TimelineText = styled.p`
+  width: 70%;
+`;
+const TimelineMainLine = styled.div`
+  height: 100%;
+  width: 4px;
+  background-color: black;
+  &.first {
+    margin-top: 115px;
+  }
+  &.last {
+    height: 50%;
+    margin-bottom: 115px;
+  }
+`;
+const TimelineSubLine = styled.div`
+  width: 8.33vw;
+  height: 4px;
+  background-color: black;
+  position: absolute;
 `;
 
 const TimelinePage = () => {
   return (
     <TimelinePageContainer container>
-      <Grid xs={2} />
-      <Grid xs={8}>
-        <TimelineCard>
-          <CardContainer>
-            <TimelineImg
-              src={neu}
-              alt="Northeastern University Logo"
-            />
-          </CardContainer>
-        </TimelineCard>
-        <TimelineCard>
-          <CardContainer>
-            <TimelineImg
-              src={harvard}
-              alt="Harvard Catalyst Logo"
-            />
-          </CardContainer>
-        </TimelineCard>
-        <TimelineCard>
-          <CardContainer>
-            <TimelineImg
-              src={loomis}
-              alt="Loomis Sayles Logo"
-            />
-          </CardContainer>
-        </TimelineCard>
+      <Grid container item>
+        <Grid xs={2} />
+        <Grid xs={1} container alignItems="center">
+          <TimelineMainLine className="first"></TimelineMainLine>
+          <TimelineSubLine></TimelineSubLine>
+        </Grid>
+        <Grid xs={7}>
+          <TimelineCard>
+            <CardContainer>
+              <TimelineImg
+                src={neu}
+                alt="Northeastern University Logo"
+              />
+              <TimelineText>
+                Vestibulum hendrerit sed erat tincidunt semper.
+                Suspendisse potenti. Mauris fermentum, est quis
+                lobortis venenatis, nisi dolor condimentum dui,
+                quis auctor risus lacus dapibus orci.
+              </TimelineText>
+            </CardContainer>
+          </TimelineCard>
+        </Grid>
+      </Grid>
+      <Grid container item>
+        <Grid xs={2} />
+        <Grid xs={1} container alignItems="center">
+          <TimelineMainLine></TimelineMainLine>
+          <TimelineSubLine></TimelineSubLine>
+        </Grid>
+        <Grid xs={7}>
+          <TimelineCard>
+            <CardContainer>
+              <TimelineImg
+                src={harvard}
+                alt="Harvard Catalyst Logo"
+              />
+              <TimelineText>
+                Vestibulum hendrerit sed erat tincidunt semper.
+                Suspendisse potenti. Mauris fermentum, est quis
+                lobortis venenatis, nisi dolor condimentum dui,
+                quis auctor risus lacus dapibus orci.
+              </TimelineText>
+            </CardContainer>
+          </TimelineCard>
+        </Grid>
+        <Grid container item>
+          <Grid xs={2} />
+          <Grid xs={1} container alignItems="center">
+            <TimelineMainLine className="last"></TimelineMainLine>
+            <TimelineSubLine></TimelineSubLine>
+          </Grid>
+          <Grid xs={7}>
+            <TimelineCard>
+              <CardContainer>
+                <TimelineImg
+                  src={loomis}
+                  alt="Loomis Sayles Logo"
+                />
+                <TimelineText>
+                  Vestibulum hendrerit sed erat tincidunt
+                  semper. Suspendisse potenti. Mauris fermentum,
+                  est quis lobortis venenatis, nisi dolor
+                  condimentum dui, quis auctor risus lacus
+                  dapibus orci.
+                </TimelineText>
+              </CardContainer>
+            </TimelineCard>
+          </Grid>
+        </Grid>
       </Grid>
       <Grid xs={2} />
     </TimelinePageContainer>
